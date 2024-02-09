c29e8df36c66dd9bd09bd1f354337559965430c1
yahiaAlg <yahialinus21@gmail.com>
2024-02-09 15:14:26 +0100

    'remove whitenoise'

--- academy_courses/wsgi.py
+++ academy_courses/wsgi.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os
 
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'academy_courses.settings')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "academy_courses.settings")
 
 application = get_wsgi_application()
